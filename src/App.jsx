9d72a657166ccc607edfc6593e8c668abaa6c68c
reaganzz <gabuteloreagan@gmail.com>
2026-02-18 10:43:25 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Vercel! 👋</h1>
+      <p>My first React app deployed on Vercel</p>
+      <p>Made with ❤️ by Christian</p>
+    </div>
+  )
+}
+
+export default App
